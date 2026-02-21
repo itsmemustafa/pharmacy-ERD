728132183f7b575fbd1d8108d7f2ddf2f1d4dbb8
mostafa <mostafamohanad321@gmail.com>
2026-02-21 23:19:00 +0300

    refactor(routes): update reports routes for new daily report endpoints

--- src/routes/reports.js
+++ src/routes/reports.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import lowQuantity from "../controllers/dailyreport/lowQuantity.js";
+import lowQuantity from "../controllers/dailyreport/lowStock.js";
+import expired from "../controllers/dailyreport/expired.js";
+import nearExpiry from "../controllers/dailyreport/nearExpiry.js";
 
 const router = Router();
 
 router.get("/low_quantity", lowQuantity);
-
+router.get("/expired", expired);
+router.get("/nearExpiry",nearExpiry)
 export default router;
