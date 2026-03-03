2a78262ecfff0d6e3be84e10d57bef4043e8c786
mostafa <mostafamohanad321@gmail.com>
2026-03-03 20:58:24 +0300

    refactor: clean up imports and formatting in auth routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,15 +4,22 @@ import signUp from "../controllers/auth/signUp.js";
 import { Router } from "express";
 import verifyEmail from "../controllers/auth/verifyEmail.js";
 import forgotPassword from "../controllers/auth/forgotPassword.js";
-import resetPassword from "../controllers/auth/resetPassword.js"; 
+import resetPassword from "../controllers/auth/resetPassword.js";
 import validate from "../middlewares/validate.js";
-import { registerSchema, loginSchema, forgotPasswordSchema, resetPasswordSchema, logoutSchema, verifyEmailSchema } from "../validations/userSchema.js";
-const router = Router();    
+import {
+  registerSchema,
+  loginSchema,
+  forgotPasswordSchema,
+  resetPasswordSchema,
+  logoutSchema,
+  verifyEmailSchema,
+} from "../validations/userSchema.js";
+const router = Router();
 
 router.post("/sign-up", validate(registerSchema), signUp);
 router.post("/login", validate(loginSchema), login);
-router.post("/logout",validate(logoutSchema),  logout);
+router.post("/logout", validate(logoutSchema), logout);
 router.get("/verify-email", validate(verifyEmailSchema), verifyEmail);
 router.post("/forgot-password", validate(forgotPasswordSchema), forgotPassword);
-router.post("/reset-password", validate(resetPasswordSchema),  resetPassword);
+router.post("/reset-password", validate(resetPasswordSchema), resetPassword);
 export default router;
